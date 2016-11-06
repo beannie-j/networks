39f4f0c72ea28542cbfed8da816860779e00a1f0
jeanniean <jeanniehjan@gmail.com>
2016-11-06 15:18:44 +0000

    Add files via upload
    Added pthreads

--- server.c
+++ server.c
@@ -8,12 +8,21 @@
 #include <sys/time.h>  
 #include <errno.h> 
 #include <string.h>
+#include <pthread.h>
 
-#define PORT8088 8000
+#define PORT8088 8080
 
 // http://localhost:8000/
+// make sure to compile with 
+// gcc server.c -o s -lpthread
+// serve multiple documents in parallel
 
-  int create_socket, new_socket;    
+  void helper_read_file(int socket);
+  void* connection_thread(void* socket_ptr);
+
+  FILE* file;
+  int g_ServerSocket; 
+  int sock, *thread_socket;   
   socklen_t addrlen;    
   int bufsize = 1024;    
   char *buffer;    // the clients request 
@@ -37,148 +46,111 @@
   " </body>\n"
   "</html>\n";
 
-
   char* not_found_response_template = 
-
   "HTTP/1.0 404 Not Found\n"
-
   "Content-type: text/html\n"
-
   "\n"
-
   "<html>\n"
-
   " <body>\n"
-
   "  <h1>Not Found</h1>\n"
-
-  "  <p>The requested URL %s was not found on this server.</p>\n"
-
+  "  <p>The requested URL was not found on this server.</p>\n"
   " </body>\n"
-
   "</html>\n";
-
-
 
   char* ok_response =
-
   "HTTP/1.0 200 OK\n"
-
   "Content-type: text/html\n"
-
-  "\n";
-
-
-
-  char* bad_method_response_template = 
-
-  "HTTP/1.0 501 Method Not Implemented\n"
-
-  "Content-type: text/html\n"
-
   "\n"
-
-  "<html>\n"
-
   " <body>\n"
-
-  "  <h1>Method Not Implemented</h1>\n"
-
-  "  <p>The method %s is not implemented by this server.</p>\n"
-
+  "  <h1>File Found</h1>\n"
+  "  <p>The requested URL was found on this server.</p>\n"
   " </body>\n"
-
   "</html>\n";
 
+  char* bad_method_response_template = 
+  "HTTP/1.0 501 Method Not Implemented\n"
+  "Content-type: text/html\n"
+  "\n"
+  "<html>\n"
+  " <body>\n"
+  "  <h1>Method Not Implemented</h1>\n"
+  "  <p>The method %s is not implemented by this server.</p>\n"
+  " </body>\n"
+  "</html>\n";
 
-
-  int bytesLeft;
-
-
-
-
+int bytesLeft;
 
 void server_createAndBind() {
-
-
-
-  if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0)
-
-    {    
-
+   if ((g_ServerSocket = socket(AF_INET, SOCK_STREAM, 0)) > 0)
+   {    
       printf("The socket was created\n");
+   } 
+   address.sin_family = AF_INET;    
+   address.sin_addr.s_addr = INADDR_ANY;    
+   address.sin_port = htons(PORT8088);    
 
-    } 
-
-    address.sin_family = AF_INET;    
-
-    address.sin_addr.s_addr = INADDR_ANY;    
-
-    address.sin_port = htons(PORT8088);    
-
-    
-
-   if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == 0)
-
-    {    
-
+   if (bind(g_ServerSocket, (struct sockaddr *) &address, sizeof(address)) == 0)
+   {    
       printf("Binding Socket\n");
-
-    }   
-
-
-
+   }   
 }
 
-
-
 void server_listen()
-
 {
-
-  if (listen(create_socket, 10) < 0) 
-
-      {    
-
+    if (listen(g_ServerSocket, 10) < 0) 
+    {    
         perror("server: listen");    
-
         exit(1);    
-
-      } 
-
+    } 
 }
 
 
 
 void server_accept() 
-
 {
-  if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) < 0) 
-      {    
-        perror("server: accept");    
-        exit(1);    
-      }    
-      if(new_socket > 0)
-      {    
-        printf("1. The Client is connected...\n");
-      }
+  int socket = accept(g_ServerSocket, (struct sockaddr *) &address, &addrlen);
+  if (socket < 0) 
+  {    
+    perror("server: accept");    
+    exit(1);
+  }   
+  int* thread_socket = (int*)malloc(sizeof(int));
+  *thread_socket = socket;
+
+  printf("Starting connection thread...\n");
+  pthread_t sniffer_thread;
+
+  if(pthread_create(&sniffer_thread, NULL, connection_thread, thread_socket) < 0) {
+    perror("Could not create thread ");
+    exit(1);
+  }
+}
+
+void* connection_thread(void* socket_ptr) {
+  printf("1. The Client is connected...\n");
+
+  int socket = *(int*)socket_ptr;
+  helper_read_file(socket);
+  printf("Closing client socket...\n");
+  close(socket);
+
+  return 0;
 }
 
 
 
-const char* server_read() {
-    memset(buffer, 0, sizeof(buffer));
-    // optimise here
-    int n = read(new_socket, buffer, 1024);
 
+
+const char* server_read(int socket) {
+    memset(buffer, 0, bufsize);
+    // optimise here
+    int n = read(socket, buffer, 1024);
     if(n < 0) 
     {
-      perror("Error ");
-	    return (const char*)0;
+      perror("server_read error ");
+      return (const char*)0;
     } 
-
     else if(n == 0) 
-
     {
       perror("Error : client disconnected");
       return (const char*)0;
@@ -192,43 +164,47 @@ const char* server_read() {
 }
 
 
-void server_write() {
-  write(new_socket, "HTTP/1.1 200 OK\n", 16);
-  write(new_socket, "Content-length: 46\n", 19);
-  write(new_socket, "Content-Type: text/html\n\n", 25);
-  write(new_socket, "<html><body><H1>The requested file was found by the server</H1></body></html>",52);    
+
+void server_write(int socket) {
+  write(socket, "HTTP/1.1 200 OK\n", 16);
+  write(socket, "Content-length: 46\n", 19);
+  write(socket, "Content-Type: text/html\n\n", 25);
+  write(socket, "<html><body><H1>The requested file was found by the server</H1></body></html>",52);    
+
 }
 
-void server_write_filenotfound()
+
+
+void server_write_filenotfound(int socket)
 {
-  write(new_socket, "HTTP/1.0 404 Not Found\n", 23);
-  write(new_socket, "Content-length: 46\n", 19);
-  write(new_socket, "Content-type: text/html\n", 24);
-  write(new_socket, "<html><body><H1>The requested file was not found by the server</H1></body></html>", 82);
+  write(socket, "HTTP/1.0 404 Not Found\n", 23);
+  write(socket, "Content-length: 46\n", 19);
+  write(socket, "Content-type: text/html\n", 24);
+  write(socket, "<html><body><H1>The requested file was not found by the server</H1></body></html>", 82);
 }
 
-void server_write_ok() {
+void server_write_ok(int socket) {
   char http_ok1[] = "HTTP/1.0 200 OK\n";
   char http_ok2[] = "Content-length: 46\n";
   char http_ok3[] = "Content-Type: text/html\n";
   char http_ok4[] = "<html><h1>File found</h1></html>\n\n";
 
-  write(new_socket, "HTTP/1.0 200 OK\n", strlen("HTTP/1.0 200 OK\n"));
-  write(new_socket, "Content-length: 46\n", strlen("Content-length: 46\n"));
-  write(new_socket, "Content-Type: text/html\n", strlen("Content-Type: text/html\n"));
-  write(new_socket, "<html><body><H1>The requested file was found</H1></body></html>",
-    strlen("<html><body><H1>The requested file was found</H1></body></html>"));
-
+  write(socket, "HTTP/1.0 200 OK\n", strlen("HTTP/1.0 200 OK\n"));
+  write(socket, "Content-length: 46\n", strlen("Content-length: 46\n"));
+  write(socket, "Content-Type: text/html\n", strlen("Content-Type: text/html\n"));
+  write(socket, "<html><body><H1>The requested file was found</H1></body></html>",
+  strlen("<html><body><H1>The requested file was found</H1></body></html>"));
 }
+
 
 
 int read_file(const char* path)
 {
     printf("Reading file %s\n", path + 1);
-    FILE* file = fopen(path + 1, "rb");
+    file = fopen(path + 1, "rb");
     if (file == NULL)
     {
-        printf("File %s not found!", path + 1);
+        printf("File %s not found!\n", path + 1);
         // TODO: Send 404
         return 0;
     } else {
@@ -248,52 +224,59 @@ int read_file(const char* path)
 }
 
 
-    
+void helper_read_file(int socket) 
+{
+    const char* message = server_read(socket);
+    printf("message : ");
+    printf("%s\n", message);
+    const char* pos = strstr(message, "\n");
+    int size = pos - message;
+    char* get = malloc(size + 1);
+    memcpy(get, message, size);
+    get[size] = 0;
+
+    int tc = 0;
+    char* token = strtok(get, " ");
+    while (token)
+    {
+        token = strtok(NULL, " ");
+        if (tc == 0)
+        {
+            if (read_file(token)) 
+            {
+              write(socket, ok_response, strlen(ok_response));
+              // Prints message in file
+              write(socket, buf, strlen(buf));
+            }  
+            else
+                write(socket, not_found_response_template, strlen(not_found_response_template));   
+        }
+        tc++;
+    }
+}
 
 int main() {   
-
   buf = (char*)malloc(size + 1);
   buffer = malloc(1024);  
   server_createAndBind();   
+  server_listen();
 
-    while (1) 
+  while (1) 
+  {    
+    server_accept(); 
+    //helper_read_file();
 
-    {    
-      server_listen();
-      server_accept();     
+  }    
 
-      const char* message = server_read();
-      printf("message : ");
-      printf("%s\n", message);
-      const char* pos = strstr(message, "\n");
-      int size = pos - message;
-      char* get = (const char*)malloc(size + 1);
-      memcpy(get, message, size);
-      get[size] = 0;
 
-      int tc = 0;
-      char* token = strtok(get, " ");
-      while (token)
-      {
-          token = strtok(NULL, " ");
-          if (tc == 0)
-          {
-              if (read_file(token)) 
-              {
-                write(new_socket, ok_response, strlen(ok_response));
-                write(new_socket, buf, strlen(buf));
-              }  
-              else
-                  write(new_socket, not_found_response_template, strlen(not_found_response_template));   
-          }
-          tc++;
-      }
-      close(new_socket);    
 
-    }    
+  close(g_ServerSocket);    
 
-  close(create_socket);    
+
 
   return 0;    
 
+
+
 }
+
